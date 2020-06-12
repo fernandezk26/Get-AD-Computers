686c0fe2119da0f8d5d04716360ce20f14ec4a76
Kevin Fernandez <33888011+fernandezk26@users.noreply.github.com>
2020-06-12 16:24:07 -0500

    Add files via upload

---
+++ Get-ADComputersByOS.ps1
@@ -0,0 +1,29 @@
+﻿#$win7Computers = Get-ADComputer -Filter {OperatingSystem -like "*Windows 7*"} ` -Properties Name, DNSHostName, OperatingSystem, `
+
+$win8Computers = Get-ADComputer -Filter {OperatingSystem -like "*Windows 8*"} ` -Properties Name, DNSHostName, OperatingSystem, `
+
+        OperatingSystemServicePack, OperatingSystemVersion, PasswordLastSet, `
+
+        whenCreated, whenChanged, LastLogonTimestamp, nTSecurityDescriptor, `
+
+        DistinguishedName |
+
+    Where-Object {$_.whenChanged -gt $((Get-Date).AddDays(-30))} |
+
+    Select-Object Name, DNSHostName, OperatingSystem, `
+
+        OperatingSystemServicePack, OperatingSystemVersion, PasswordLastSet, `
+
+        whenCreated, whenChanged, `
+
+        @{name='LastLogonTimestampDT';`
+
+          Expression={[datetime]::FromFileTimeUTC($_.LastLogonTimestamp)}}, `
+
+        @{name='Owner';`
+
+          Expression={$_.nTSecurityDescriptor.Owner}}, `
+
+        DistinguishedName
+
+        $win8Computers | Export-Csv -Path C:\scripts\computers.csv
